f998cbfde56f8d90c7dab9288a3855cddfe1ea49
ragnareck <ranareck@live.com>
2017-12-21 19:05:03 +0300

    asda

--- 7.3.py
+++ 7.3.py
@@ -1,10 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
+import sys
+#import numpy 
+vlan = int(input('vvedite vlan: '))
+if type(vlan) != int:
+    print('razyi glaza')
+    sys.exit()
+array = list()
 with open('CAM_table.txt', 'r') as macs:
     for string in macs:
         if string.count('Gi') > 0:
 #            print(string.rstrip())
-            string.split().pop(2)
-            print(string)
-
+            perem = string.split()
+            del perem[2]
+            array.append(perem)
+    array.sort()
+    for stri in array:
+        print '   '.join(stri)
+        print(stri[0])
+        if int(stri[0]) != int(vlan):
+            print(type(vlan))
+            print int(stri[0])
+            print('nety')
+            sys.exit()
+        else:
+            print stri
 
